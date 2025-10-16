79539f822bf0137b1a8b7a4ec63378bdc49f1f10
l3r8yJ <l3r8y@duck.com>
2025-10-16 19:40:01 +0300

    chore(#24): remove extended scalars

--- common-scalars-starter/build.gradle.kts
+++ common-scalars-starter/build.gradle.kts
@@ -4,6 +4,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("com.graphql-java:graphql-java:24.3")
 
-    api("com.netflix.graphql.dgs:dgs-starter")
-    api("com.netflix.graphql.dgs:graphql-dgs-extended-scalars")
+    implementation("com.netflix.graphql.dgs:dgs-starter")
 }
