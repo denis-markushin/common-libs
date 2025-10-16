2959bfc51cb01f116ea705d33679ba759eb9a175
l3r8yJ <l3r8y@duck.com>
2025-10-16 17:28:08 +0300

    chore(#24): remove project dependency

--- graphql-dgs-starter/build.gradle.kts
+++ graphql-dgs-starter/build.gradle.kts
@@ -5,7 +5,6 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter")
 
-    api(project(":common-scalars-starter"))
     api("com.netflix.graphql.dgs:dgs-starter")
     api("com.netflix.graphql.dgs:graphql-dgs-spring-boot-micrometer")
     api("com.netflix.graphql.dgs:graphql-dgs-pagination")
