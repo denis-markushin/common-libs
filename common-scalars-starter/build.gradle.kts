cfba1cbcb163b140b1934ad01013312d22f7c029
l3r8yJ <l3r8y@duck.com>
2025-10-21 00:17:36 +0300

    chore(#24): change description, remove redundant dependency

--- common-scalars-starter/build.gradle.kts
+++ common-scalars-starter/build.gradle.kts
@@ -1,8 +1,6 @@
-description = "Spring Boot starter that simplifies building GraphQL APIs with Jooq and DGS Framework"
+description = "A Spring Boot GraphQL starter that provides several useful scalar types."
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("com.graphql-java:graphql-java:24.3")
-
     implementation("com.netflix.graphql.dgs:dgs-starter")
 }
